83295ded53f9602fed9f4c573cd97b6c58725362
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-10-19 11:06:25 -0400

    get attendance, by day, by a specified team

--- lib/elixir_mlb_attendance.ex
+++ lib/elixir_mlb_attendance.ex
@@ -24,9 +24,27 @@ defmodule ElixirMlbAttendance do
     # build list of {team, attendance} tuples
     |>
     team_attendance_list
+  end
+
+  def attendance_by_day_for_team(team) do
+    build_data_store
+
+    |>
+    get_days
+
+    |> 
+    Enum.map(fn(dow) -> 
+      all_records = build_data_store
+      day_records = Enum.filter(all_records, &(Enum.at(&1,1) == dow and Enum.at(&1,2) == team))
+      day_total = get_total_attendance(day_records) 
+      {dow, day_total, (day_total/Enum.count(day_records))}
+
+
+      end)
 
     
   end
+
 
   defp team_attendance_list(teams) do
     all_attendance = build_data_store
@@ -53,6 +71,16 @@ defmodule ElixirMlbAttendance do
     _get_teams(Set.put(acc, Enum.at(head,2)), tail)
   end
 
+  defp get_days(list) do
+    _get_days HashSet.new,list
+  end
+
+  defp _get_days(acc, []) do 
+    Set.to_list(acc)
+  end
+  defp _get_days(acc, [head | tail]) do
+    _get_days(Set.put(acc, Enum.at(head,1)), tail)
+  end
   
   defp get_total_attendance(list) do
     Enum.reduce(list, 0, fn(x, acc) -> 
@@ -76,7 +104,6 @@ defmodule ElixirMlbAttendance do
 
     |>
     remove_attendance_non_integers
-
   end
 
 
